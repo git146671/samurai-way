b51d373bad1892cd7e3ea7e3ce39a6e11ad2be6f
Anton Plekhanov <146661@mail.ru>
2023-12-23 12:39:40 +0300

    Lesson-11

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/Header";
+import {Technologies} from "./components/Technologies";
+import {Footer} from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
