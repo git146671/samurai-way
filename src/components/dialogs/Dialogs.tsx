ac2be5715a55187d421b8059209b014003ab7906
Anton Plekhanov <146661@mail.ru>
2023-12-29 00:31:18 +0300

    Lesson-21

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,6 +1,30 @@
+import styled from "styled-components";
 
 export const Dialogs = () => {
-    return <div>
-        Dialogs
-    </div>
+    return <StyledDialogs>
+        <StyledDialogsItems>
+            <div>Friend1</div>
+            <div>Friend2</div>
+            <div>Friend3</div>
+            <div>Friend4</div>
+        </StyledDialogsItems>
+        <StyledMessages>
+            <div>msg1</div>
+            <div>msg2</div>
+            <div>msg3</div>
+        </StyledMessages>
+    </StyledDialogs>
 }
+
+const StyledDialogs = styled.div`
+    display: grid;
+    grid-template-columns: 2fr 10fr;
+    `
+
+const StyledDialogsItems = styled.div`
+    padding: 10px;
+    `
+
+const StyledMessages = styled.div`
+    padding: 10px;
+    `
